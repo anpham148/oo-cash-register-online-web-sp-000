9ffc4d929f18c14ac01ac3c4b836a97166ebec26
An Pham <anpham148@gmail.com>
2020-07-07 11:54:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
+class CashRegister
+  attr_accessor :cash_register, :discount, :total
 
+  def initialize (discount = nil)
+    @total = 0
+    @discount = discount
+
+  end
+
+  def add_item(title, price, quantity = 1)
+    @title = title
+    @total += (price * quantity)
+    @quantity = quantity
+  end
+
+  def apply_discount
+    @total - (@total * (@discount / 100))
+    
+  end
+
+end
