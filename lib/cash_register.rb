ee3f4eaabe808d599f1b7a899b576c90a4e11e89
An Pham <anpham148@gmail.com>
2020-07-07 12:30:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   def apply_discount
     @total = @total - (@total * (@discount / 100.0))
     @total = @total.round
-    message = "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total}."
 
   end
 
